455d5e89cb15288828bebc97f5a78d382cc366cb
Mohd Iliyas <mohdiliyas4614@gmail.com>
2025-08-04 00:10:34 +0530

    changes made

--- backend/app.py
+++ backend/app.py
@@ -100,5 +100,5 @@ def get_predictions():
 def map_view():
     return render_template('map.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
